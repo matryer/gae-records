cee9ddf3d11fa429c26794177ed527e11edbfe5d
matryer <matryer@mac.com>
2012-02-06 18:02:31 -0700

    added home grown Panic method

--- gaerecords/errors.go
+++ gaerecords/errors.go
@@ -2,6 +2,7 @@ package gaerecords
 
 import (
 	"os"
+	"fmt"
 )
 
 // The following errors are defined in this package:
@@ -10,3 +11,7 @@ var (
 	// Error returned when an event callback cancels an operation
 	ErrOperationCancelledByEventCallback = os.NewError("gaerecords: Operation was cancelled by an event callback.")
 )
+
+func Panic(message string) {
+	panic(fmt.Sprint("gaerecords: ", message))
+}
